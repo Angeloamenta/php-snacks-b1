444bdcbba8fc853c46be5fa8a46046f2b31289ae
Angeloamenta <angeloamenta7@gmail.com>
2022-02-04 16:18:09 +0100

    chiamata completata

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!',
+      cars : [],
+    },
+    created() {
+        // ho dovuto resettare le porte su mamp, all'inizio non trovava le api ma facendo vari tentativi il problema era la cartella "boolean" di troppo nel percorso 
+        axios.get('http://localhost:8888/php-ajax-dischi/server/controller-api.php').then((result) => {
+          console.log(result.data.result);
+          this.cars = result.data.result;
+        }).catch((error) => { console.log(error); });
+      }
+  })
